7a3e7cd73720997f6aa75ee8d9b8e485ecc7034f
João Pedro Souza W. <158215503+jotaPwt@users.noreply.github.com>
2024-10-18 17:49:43 -0300

    Add files via upload

---
+++ aula3_linguagens-da-programação_exercicios.py
@@ -0,0 +1,162 @@
+'''
+
+print('Hello World')
+
+# variaveis de texto (string)
+
+nome = 'joao'
+_nome2 = 'carlos'
+
+#declarações semanticas
+
+
+#snakecase
+
+nome_do_usuario = 'pedro'
+
+preco_do_produto = 10
+
+#camecase
+
+nomeDoUsuario = 'joao'
+
+#float numero reais
+
+real = 10.5
+real_2 = 20.2
+
+
+#interger (numero inteiro)
+
+numero_inteiro = 5
+numero_inteiro2 = 30
+numero_inteiro3 = 100
+
+#number bool (true or false)
+
+verdadeiro = True
+falso = False
+
+#VARIAVEIS CONSTANTES (variavel em letra maiuscula) elas não se alteram
+
+NOME = 'joao'
+NOME = 'TEST'
+
+print(NOME)
+#as variaveis se alteram, mesmo sendo "constantes"
+
+'''
+'''
+#------------------------------------------
+
+#funcao - ação
+
+print('mostrar no ambiente de teste')
+
+#numero = 10
+#text = 'algo algho'
+#real = 10.5
+#bool = True
+
+
+#numero = 10
+#tipo2 = type(text)
+#tipo3 = type(real)
+#tipo4 = type(bool)
+
+
+#print(tipo2)
+#print(tipo3)
+#print(tipo4)
+
+int = 10
+
+inteiro_em_real = float(int)
+
+print(inteiro_em_real)
+
+tipo = type(inteiro_em_real)
+print(tipo)
+
+
+num = 50
+
+print(num)
+
+numero_para_string = str(num)
+
+print(type(num))
+
+#-------------------------------------
+'''
+-----
+
+'''
+#nome = input('digite seu nome:')
+
+n1 = float(input('>> '))
+n2 = int(input('>> '))
+
+N2 = float(n2)
+
+soma = n1 + N2
+print(soma)
+
+'''
+
+
+
+
+#atividade de calculadora
+
+print('SOMA')
+n1 = int(input('digite um numero'))
+n2 = int(input('digite outro numero'))
+         
+soma = n1 + n2
+print(soma)
+
+
+#multiplicação
+print('MULT')
+n3 = int(input('digite um numero'))
+n4 = int(input('digite outro numero'))
+multiplicacao = n3 * n4
+print(multiplicacao)
+
+
+#divisao
+print('DIVISAO')
+n5 = int(input('digite um numero'))
+n6 = int(input('digite outro numero'))
+
+tipoFloatN5 = float(n5)
+tipoFloatN5 = float(n6)
+
+
+divisao = n5 / n6
+print(divisao)
+
+
+#subtração
+
+print('SUBTRACAO')
+n7 = int(input('digite um numero'))
+n8 = int(input('digite outro numero'))
+
+subtracao = n7 - n8
+print(subtracao)
+
+
+#condicional
+
+#1 - SISTEMA PRECISA VERFICAR SE O USUÁRIO TEM VALOR NO CC 
+#2 - DIGITE O DADOS DO CARTÃO 
+#3 - PROCESSAMENTO DE ENTRADAS DOS DADOS 
+#4 - MOSTRAR SE O USUARIO VAI TER CREDITO PARA PAGAR A CONTA 
+#5 - FINALIZAR A COMPRA
+
+
+
+
+
